14231deccef9ef17860e150aebce3724dbef91d4
anhishek1289@gmail.com <abhishek1289@gmail.com>
2022-12-14 21:17:39 +0530

    JS

---
+++ main.js
@@ -0,0 +1,22 @@
+// function newfunction(a, b){
+//     console.log(a+b);
+// }
+// function newfunction(a, b){
+//     console.log(a+b);
+// }
+// let x = myFunction(4, 3);   
+
+// function myFunction(a, b) {
+//   console.log(a+b);             
+// } 
+
+// function factorial(n){
+//   let fact = 1;
+//   for(let i = n; i >= 1; i--){
+//     fact*=i;
+//   }
+//   return (fact);
+// }
+// console.log(factorial(5));
+
+
